6a89c19c360b18b59a8014f54f37bbd0812bc60d
Curtis Cawley <curtis.cawley@icloud.com>
2018-03-23 15:52:56 -0700

    curts at the wheel, justin in his ear, crushing it

--- controllers/commits.controller.js
+++ controllers/commits.controller.js
@@ -1,4 +1,5 @@
 const { Commit } = require('../models')
+const axios = require('axios')
 
 class CommitsController {
   constructor() {}
@@ -23,15 +24,40 @@ class CommitsController {
       })
   }
 
-  // static create (req, res, next) {
-  //   Commit.create()
-  //     .then(commits => {
-  //       return res.json({ commits })
-  //     })
-  //     .catch(err => {
-  //       console.log('Error!', err);
-  //     })
-  // }
+  static create (req, res, next) {
+    // axios call hell begins here...
+    //gets all repos for a specific user as an array
+        // loop over array and hunt for 'full_name' (that's the username and reponame)
+
+    // https://api.github.com/repos/just-hey/g-snacks-client/commits
+    //gets all commits for a specific repo as an array
+    console.log("here bitches");
+    console.log(req.body.username);
+    axios.get(`https://api.github.com/users/${req.body.username}/repos`)
+      .then(result => {
+        let githubInfo = result.data.map(repo => {
+          return repo.full_name
+        })
+        return githubInfo
+      })
+      .then(githubInfo => {
+        // console.log(githubInfo);
+        let commitsArray = githubInfo.map(repo => {
+          return axios.get(`https://api.github.com/repos/${repo}/commits`)
+            .then(result => {
+              console.log(result.data);
+              return result
+            })
+        })
+      })
+    // Commit.create()
+    //   .then(commits => {
+    //     return res.json({ commits })
+    //   })
+    //   .catch(err => {
+    //     console.log('Error!', err);
+    //   })
+  }
 
 }
 
