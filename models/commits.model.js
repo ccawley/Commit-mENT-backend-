656c57b1738d61db592ee34343e122f10d7f0b44
Justin Hays <jhays124@yahoo.com>
2018-03-23 14:41:32 -0700

    commit routes in process

--- models/commits.model.js
+++ models/commits.model.js
@@ -6,12 +6,31 @@ const bcrypt = require('bcrypt')
 class Commit {
   constructor() {}
 
-  static index() {
-    console.log('model');
+  static index () {
+    console.log('model')
     // should return all commits from every user!
     return knex('commits')
+  }
+
+  //take in username, get OUR user_id from 'users' table, use that id to get all commits with that ID
+  static userCommits (username) {
+    return knex('users')
+      .where({ user_name: username })
+      .first()
+      .then(userInfo => {
+        return knex('commits')
+          .where({ user_id: userInfo.id })
+      })
   }
 
 }
 
 module.exports = Commit
+
+
+// https://api.github.com/users/just-hey/repos
+//gets all repos for a specific user as an array
+    // loop over array and hunt for 'full_name' (that's the username and reponame)
+
+// https://api.github.com/repos/just-hey/g-snacks-client/commits
+//gets all commits for a specific repo as an array
