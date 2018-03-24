a31f5259798cd1209f3da6cf5217547388b22db8
Anh Trieu <anh.trieu@icloud.com>
2018-03-24 02:08:25 -0700

    deleted some more console.log

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,8 +5,6 @@ class AuthController {
 
   static prepParams(req, res, next) {
     let {code, state} = req.query
-    console.log('code', code)
-    console.log('state', state)
     let params = {
       client_id:process.env.CLIENT_ID,
       client_secret:process.env.CLIENT_SECRET,
@@ -14,7 +12,6 @@ class AuthController {
       redirect_uri: 'http://commit-m.surge.sh',
       state: state
     }
-    // res.status(200).json({message: 'hey team!'})
     Token.tokenExchange(params)
       .then(data => res.status(200).json(data))
   }
