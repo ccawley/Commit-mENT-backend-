479cc37ec553735c285820061f622b92522da745
Anh Trieu <anh.trieu@icloud.com>
2018-03-27 16:54:37 -0700

    changed some more

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -15,10 +15,9 @@ class AuthController {
     console.log('This is BODY', params)
     Token.tokenExchange(params)
       .then(data => {
-        console.log('ctrl data is actually.....',data);
         res.status(200).send(data)
       })
-      .catch(err)
+      .catch(err => console.log(err))
   }
 }
 
