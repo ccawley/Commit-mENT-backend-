60bd0c94dfd995662de7b5edf5d17192a0fdd4b8
Justin Hays <jhays124@yahoo.com>
2018-03-27 13:05:01 -0700

    removed the magic

--- app.js
+++ app.js
@@ -6,38 +6,10 @@ const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000
 require('dotenv').config()
 
-
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-  app.use(allowCrossDomain);
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-  app.use(cors())
-  app.disable('x-powered-by')
-  app.use(morgan('dev'))
-  app.use(bodyParser.json())
-
-app.all('*', function(req, res, next) {
-     var origin = req.get('origin');
-     res.header('Access-Control-Allow-Origin', origin);
-     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-     res.header('Access-Control-Allow-Headers', 'Content-Type');
-     next();
-})
+app.use(cors())
+app.disable('x-powered-by')
+app.use(morgan('dev'))
+app.use(bodyParser.json())
 
 const { UsersRouter, AuthRouter, CommitsRouter } = require('./routes')
 app.use('/users', UsersRouter)
