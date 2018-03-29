51f9e1ddb1eb029b28bf9d51f7dcf390cb61c42e
Anh Trieu <anh.trieu@icloud.com>
2018-03-28 17:20:12 -0700

    finished GET/ALL commits route

--- models/commits.model.js
+++ models/commits.model.js
@@ -5,19 +5,27 @@ class Commit {
   constructor() {}
 
   static index () {
-    console.log('model')
     // should return all commits from every user!
-
     return knex('users')
       .then(users => {
-        users.forEach(user => {
-          return knex('users')
-            .join('commits')
-            .where('users_id', 'users.id')
-            .andWhere('users.id', user.id)
-            .select('full_name', 'user_name', 'avatar_image', 'created_on', 'message')
-            .then(result => console.log(result))
+        let bfp = users.map(user => {
+          return knex.raw(`SELECT full_name, user_name, avatar_image, created_on, message FROM users, commits WHERE user_id=users.id AND users.id=${user.id}`)
+          .then(result => result.rows)
+        //   return knex('users')
+        //     .join('commits')
+        //     .select('users.full_name', 'users.user_name', 'users.avatar_image', 'commits.created_on', 'commits.message')
+        //     .where('commits.user_id', 'users.id')
+        //     .andWhere('users.id', user.id)
+        //     .then(result => console.log(result))
         })
+        return Promise.all(bfp)
+          .then(result => {
+            let flattened = []
+            result.forEach(user => {
+              user.forEach(commit => flattened.push(commit))
+            })
+            return flattened
+          })
       })
 
   }
